b509cfbf1b3975d384fc4b5b20c711c139115032
DiegoVillanueva32 <d.villanueva0504@gmail.com>
2025-03-26 19:18:01 -0500

    Update Lenguage.java

--- src/Lenguage/Lenguage.java
+++ src/Lenguage/Lenguage.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+package Lenguage;
 
 public class Lenguage {
+    protected String name ;
+    protected int numSpeakers;
+    protected String regionSpoken;
+    protected String wordOrder;
+
+    public Lenguage (String name,int numSpeakers,String regionSpoken,String wordOrder ){
+        this.name = name;
+        this.numSpeakers = numSpeakers;
+        this.regionSpoken = regionSpoken;
+        this.wordOrder = wordOrder;
+    }
+    public void getInfo(){
+        System.out.println(this.name + " is spoken by " + this.numSpeakers + " people mainly in "+ this.regionSpoken + ".");
+        System.out.println("the language follows the word order by " + this.wordOrder + ".");
+    }
 }
