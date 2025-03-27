a140874c417a74bb900e631c1505ad11612c79cc
DiegoVillanueva32 <d.villanueva0504@gmail.com>
2025-03-26 19:19:14 -0500

    Update SinoTibetian.java

--- src/Lenguage/SinoTibetian.java
+++ src/Lenguage/SinoTibetian.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+package Lenguage;
 
-public class SinoTibetian {
+public class SinoTibetian extends Lenguage{
+    public SinoTibetian(String name, int numSpeakers) {
+        super(name, numSpeakers, "Asia", "subject - object - verb");
+        if (this.name.equals("Chinese")){
+            this.wordOrder = "subject - object";
+        }
+    }
 }
