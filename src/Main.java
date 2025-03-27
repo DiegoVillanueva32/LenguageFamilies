b843e2da33627636299ebaefdea16c4a0bf280f5
DiegoVillanueva32 <d.villanueva0504@gmail.com>
2025-03-26 19:19:35 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import Lenguage.*;
 
 public class Main {
+    public static void main(String[] args) {
+        Lenguage myLang = new Lenguage ("Chibchombiano", 5000000,"south America", "Sujeto - Verbo - Predicado ");
+        myLang.getInfo();
+
+        Mayan akatek = new Mayan("Akatek", 45430);
+        akatek.getInfo();
+
+        SinoTibetian mandarin = new SinoTibetian("Mandarin", 1140000000);
+        mandarin.getInfo();
+
+        SinoTibetian burmese = new SinoTibetian("Burmese", 42900000);
+        burmese.getInfo();
+    }
 }
